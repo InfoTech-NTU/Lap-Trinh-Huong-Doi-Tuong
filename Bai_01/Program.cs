6cb367b683c6e825a3c5fd0ba43c970ff88f9ae7
InfoTech-NTU <clbtinhoc.kcntt@ntu.edu.vn>
2025-05-21 20:03:33 +0700

    Add files via upload

---
+++ Bai_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using BTON1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            DsXe dsXe = new DsXe();
+            dsXe.Nhap();
+            dsXe.sapXep();
+            dsXe.XuatDS();
+            dsXe.themXe();
+            dsXe.XuatDS();
+            dsXe.xoaDongXe();
+            dsXe.XuatDS();
+            Console.ReadKey();
+        }
+    }
+}
